dc5e3cad241b28322d4f4743d9dc7754fdfd7f00
Vahe Sevachyan <vscode13@gmail.com>
2025-03-13 17:47:35 -0700

    recreated project with tailwind/sass

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<template>
+  <div class="bg-green-500 p-4 text-white">
+    <p class="text-xl">Hello, Vue with Tailwind and SASS!</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+$primary-color: #94a99e;
+
+p {
+  color: $primary-color;
+  font-weight: bold;
+}
+</style>
