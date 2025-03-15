945d5f83038a06ef6e04c838f605cef58948737f
Vahe Sevachyan <vscode13@gmail.com>
2025-03-15 12:32:25 -0700

    updated listings component for loop

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,42 @@
 <script setup>
+import { ref, reactive } from "vue";
 import Card from "./components/Card.vue";
 import Listings from "./components/Listings.vue";
+import roleData from "./assets/data.json";
+const roles = ref(roleData);
 </script>
 
 <template>
   <div class="nav-section-container">
     <div class="title-container">
-      <h1>Welcome to Dev Roles</h1>
+      <!-- <h1>Welcome to Dev Roles</h1> -->
     </div>
     <div class="search-field-container">
       <input type="text" />
     </div>
-    <h1 class="text-3xl font-bold underline">Hello from vue</h1>
+    <!-- <h1 class="text-3xl font-bold underline">Hello from vue</h1> -->
   </div>
-  <Listings />
+  <Listings :roles="roles" />
   <!-- <Card /> -->
 </template>
 
 <style lang="scss">
-$secondary-color: #2985a9;
+// $secondary-color: #2985a9;
 
-.container {
-  padding: 20px;
-  // background-color: $secondary-color;
-  h1 {
-    color: rgb(153, 30, 30);
-  }
-}
-.nav-section-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  background-color: $secondary-color;
-  width: 50%;
-  margin: auto;
-}
+// .container {
+//   padding: 20px;
+//   background-color: $secondary-color;
+//   h1 {
+//     color: rgb(153, 30, 30);
+//   }
+// }
+// .nav-section-container {
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   flex-direction: column;
+//   background-color: $secondary-color;
+//   width: 50%;
+//   margin: auto;
+// }
 </style>
--- src/components/Listings.vue
+++ src/components/Listings.vue
@@ -1,36 +1,56 @@
 <template>
-  <div className="flex items-center justify-center min-h-screen bg-gray-900">
+  <div class="flex items-center justify-center min-h-screen bg-gray-900">
     <div
-      className="bg-white p-3.5 rounded-2xl shadow-lg w-175 text-center flex items-center justify-between"
+      class="bg-white p-3.5 rounded-2xl shadow-lg w-175 text-center flex items-center justify-between"
     >
-      <div class="flex items-center">
-        <img src="../assets/images/eyecam-co.svg" alt="" class="size-15 mr-2" />
+      <div class="flex items-center" v-for="role in roles" :key="role.id">
+        <img :src="role.logo" alt="" class="size-15 mr-2" />
         <div class="flex-row-reverse">
           <div class="flex items-center justify-start w-60 border-1">
-            <span class="mr-2">Photoshop</span>
-            <span class="mr-2">New</span>
-            <span>Featured</span>
+            <span class="mr-2">{{ role.company }}</span>
+            <span class="mr-2">{{ role.new }}</span>
+            <span>{{ role.featured }}</span>
           </div>
-          <h2
-            className="flex text-xl font-semibold text-gray-800 justify-start"
-          >
-            Full Stack Developer
+          <div class="flex items-center justify-start w-60 border-1">
+            <span>{{ role.company }}</span>
+            <span>{{ role.new }}</span>
+            <span>{{ role.featured }}</span>
+          </div>
+          <h2 class="flex text-xl font-semibold text-gray-800 justify-start">
+            {{ role.position }}
           </h2>
           <div class="flex items-center justify-start w-70 border-1">
-            <span class="mr-2">Post Date</span>
-            <span class="mr-2">Contract Type</span>
-            <span>Location</span>
+            <span class="mr-2">{{ role.postedAt }}</span>
+            <span class="mr-2">{{ role.contract }}</span>
+            <span>{{ role.location }}</span>
           </div>
         </div>
       </div>
-
       <div class="flex items-center justify-around w-60">
+        <span
+          v-for="(language, index) in roles.languages"
+          :key="index"
+          class="bg-gray-200 text-orange-300 px-2 py-1 rounded-full mr-2"
+        >
+          {{ language }}
+        </span>
+      </div>
+      <!-- <div class="flex items-center justify-around w-60">
+        <span v-for="language in roles.languages" :key="language">{{
+          language
+        }}</span>
+
         <span>Vuejs</span>
         <span>JavaScript</span>
         <span>Python</span>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
-<script></script>
+<script setup>
+import { defineProps } from "vue";
+defineProps({
+  roles: Array,
+});
+</script>
 <style scoped></style>
